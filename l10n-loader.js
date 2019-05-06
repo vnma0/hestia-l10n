c789bd9c689afeed2c83d75cb6b040e7185a98f8
Minh Duc Vo <minhducsun2002@gmail.com>
2019-05-06 13:16:15 +0700

    Convert translation object key to support i18next

--- l10n-loader.js
+++ l10n-loader.js
@@ -2,8 +2,8 @@ import vi_VN from './global/vi-VN.json';
 import en_US from './global/en-US.json';
 
 let out = {
-    vi_VN: { display: vi_VN.display, resources: vi_VN.resources },
-    en_US: { display: en_US.display, resources: en_US.resources }
+    vi_VN: { display: vi_VN.display, translation: vi_VN.resources },
+    en_US: { display: en_US.display, translation: en_US.resources }
 };
 
 let supportedLanguages = {
